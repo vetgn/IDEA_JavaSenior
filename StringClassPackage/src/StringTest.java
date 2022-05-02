61e6aa07e44f9a4f6c620d76e41fce8706482438
ZJJ <2175939466@qq.com>
2022-05-02 22:34:39 +0800

    2022.5.2

---
+++ StringClassPackage/src/StringTest.java
@@ -0,0 +1,26 @@
+import org.junit.Test;
+
+/**
+ * String 的使用
+ *
+ * @author ZJJ
+ * #Description StringTest
+ * #Date: 2022/5/2 19:49
+ */
+public class StringTest {
+
+    /*
+     * String :字符串，使用一对""引起来表示
+     * 1.String声明为final的，不可被继承
+     * 2.String实现了Serializable接口：表示字符串是支持序列化的。
+     *         实现了Comparable接口：表示String可以比较大小
+     * 3.String内部定义了·final char[] value用于存储字符串大小
+     * 4.String：代表不可变的字符序列。简称：不可变性。
+     *      体现：
+     * */
+    @Test
+    public void test1() {
+
+    }
+}
+
